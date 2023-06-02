31f44ac2d6afa0d15e03d1a9618ab85a76ca8aa0
NachoArmen <ignacio.armendariz@davinci.edu.ar>
2023-06-02 15:36:15 -0300

    feat: cart done

--- cart.php
+++ cart.php
@@ -1,9 +1,93 @@
 <?php
 include_once "header.php";
 ?>
+<br>
 
-<h1 class="cali">Carrito en desarrollo</h1>
+<h1 class="cali">¿Ready to check out?</h1>
 
+<section>
+<div class="container-fluid">
+<h2 class="cali">Payment Information</h2>
+    <div class="row">      
+        <form action="POST">
+        <div class="row">  
+            <div class="col">
+                <div class="col">
+                    <p class="cali">Card Holder: </p>
+                    <input type="text" name="" id="">
+                </div>
+                <div class="col">
+                    <p class="cali">Card Number</p>
+                    <input type="number" name="" id="">
+                </div>
+                <div class="col">
+                    <p class="cali">Expire Date</p>
+                    <input type="text" name="" id="">
+                </div>
+            </div>
+            <br>
+            <div class="col">
+                <div>
+                    <p class="cali">Surname: </p>
+                    <input type="text" name="" id="">
+                </div>
+                <div>
+                    <p class="cali">CVV</p>
+                    <input type="number" name="" id="">
+                </div>
+                <div>
+                    <p class="cali">Card Type</p>
+                    <input type="text" name="" id="">
+                </div>
+            </div> 
+             
+         </div>            
+    </div>
+<h2 class="cali">Shipping Information</h2>
+        <div class="row">
+            <div class="row">
+            <div class="col">
+                <div class="col">
+                    <p class="cali">Country: </p>
+                    <input type="text" name="" id="">
+                </div>
+                <div class="col">
+                    <p class="cali">Street: </p>
+                    <input type="number" name="" id="">
+                </div>
+                <div class="col">
+                    <p class="cali">Extra Info.:</p>
+                    <input type="text" name="" id="">
+                </div>
+            </div>
+            <br>
+            <div class="col">
+                <div>
+                    <p class="cali">State/Province:</p>
+                    <input type="text" name="" id="">
+                </div>
+                <div>
+                    <p class="cali">Street Number</p>
+                    <input type="number" name="" id="">
+                </div>
+                <div>
+                    <p class="cali">Courier</p>
+                    <input type="text" name="" id="">
+                </div>
+            </div>
+
+            </div>
+            
+        </div> 
+        <br>
+            <div class="col send">
+                <input type="submit" id="sub" >
+            </div> 
+    </form>
+</div>
+
+</section>
+<br>
 <?php
 include_once "footer.php";
 ?>  
